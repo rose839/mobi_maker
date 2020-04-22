001755d8fb85bf7ec0271d391f6dd4a2d08c03cf
rose839 <617418745@qq.com>
2020-04-23 00:12:21 +0800

    add class Ebook

--- mobi_maker/ebooklib.py
+++ mobi_maker/ebooklib.py
@@ -1,5 +1,51 @@
 import os
 import io
+import pathlib
+import tempfile
+import time
+
+def format_file_name(path: str) -> str:
+    """
+    strip illegal characters
+    """
+    return path.replace('/', '').replace(' ', ''). \
+        replace('+', '-').replace('"', '').replace('\\', ''). \
+        replace(':', '-').replace('|', '-')
+
+class Ebook:
+    def __init__(self, title: str, author=None):
+        self.title = title
+        self.author = author
+        self.cover_path = None
+        self.cover_content = None
+        self.chapter_list = list()
+        self._tempdir = pathlib.Path(tempfile.gettempdir()) / str(time.time())
+        self._tempdir.mkdir()
+
+    @property
+    def dest_file_path(self) -> pathlib.Path:
+        return pathlib.Path('{}.mobi'.format(format_file_name(self.title)))
+
+    @property
+    def tmpdir(self) -> pathlib.Path:
+        return self._tempdir
+
+    @property
+    def full_dest_file_path(self) -> pathlib.Path:
+        return self.tempdir / self.dest_file_path
+
+    def set_cover(self, cover_path: str = None, cover_content: bytes = None) -> 'Ebook':
+        if cover_path:
+            self.cover_path = cover_path
+            self.cover_content = None
+            return
+        if cover_content:
+            self.cover_content = cover_content
+            self.cover_path = None
+            return
+        return ValueError('cover image path and content both are empty.')
+
+
 
 def _parse_headers(toc_file_name):
     """
@@ -34,7 +80,9 @@ def _parse_headers(toc_file_name):
             elif h.startswith('## '):
                 if len(headers_info) == 0:
                     raise ValueError("invalid toc md file: format not correct")
-                headers_info[-1]['next_headers'].append({  # only suport 2 level toc
+
+                # only suport 2 level toc
+                headers_info[-1]['next_headers'].append({
                     'title': h[3:].strip(),
                 })
         if not headers_info:
@@ -50,8 +98,11 @@ def make_ebook(source_dir, output_dir):
     :param output_dir:
     :return: destination filename of the mobi file
     """
+    # parse toc.md file
     toc_file_name = os.path.join(source_dir, "toc.md")
     title, headers = _parse_headers(toc_file_name)
+
+
 
 if __name__ == '__main__':
     print(_parse_headers('../example/toc.md'))
